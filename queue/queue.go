e36a1979aeb183e6c0316ec24dec9e74831cda12
vexodeus <vexodeus@gmail.com>
2023-05-31 10:39:55 +0300

    add edge cases handling

--- queue/queue.go
+++ queue/queue.go
@@ -1,4 +1,4 @@
-package collection
+package queue
 
 type Queue struct {
 	Slice []any
@@ -11,13 +11,23 @@ func (q *Queue) Len() int {
 	return len(q.Slice)
 }
 func (q *Queue) Peek() any {
+	if q.IsEmpty() {
+		return nil
+	}
 	return q.Slice[0]
 }
 func (q *Queue) Enqueue(value any) {
 	q.Slice = append(q.Slice, value)
 }
 func (q *Queue) Dequeue() any {
+	if q.IsEmpty() {
+		return nil
+	}
 	res := q.Slice[0]
+	if q.Len() == 1 {
+		q.Slice = []any{}
+		return res
+	}
 	q.Slice = q.Slice[1:]
 	return res
 }
--- stack/stack.go
+++ stack/stack.go
@@ -11,13 +11,23 @@ func (s *Stack) Len() int {
 	return len(s.Slice)
 }
 func (s *Stack) Peek() any {
+	if s.IsEmpty() {
+		return nil
+	}
 	return s.Slice[:s.Len()]
 }
 func (s *Stack) Push(value any) {
 	s.Slice = append(s.Slice, value)
 }
 func (s *Stack) Pop() any {
+	if s.IsEmpty() {
+		return nil
+	}
 	res := s.Peek()
+	if s.Len() == 1 {
+		s.Slice = []any{}
+		return res
+	}
 	s.Slice = s.Slice[:s.Len()]
 	return res
 }
